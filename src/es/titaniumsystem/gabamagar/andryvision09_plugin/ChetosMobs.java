186b0a6de8966053250fdde1a14e153f719654d4
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2017-05-23 00:52:24 +0200

    avances minimos

--- src/es/titaniumsystem/gabamagar/andryvision09_plugin/ChetosMobs.java
+++ src/es/titaniumsystem/gabamagar/andryvision09_plugin/ChetosMobs.java
@@ -1,11 +1,28 @@
 package es.titaniumsystem.gabamagar.andryvision09_plugin;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Monster;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChetosMobs extends JavaPlugin {
 
+	public Map<String, Map<String, Double>> ajustes;
+	public Set<String> mobs;
+	
+
 	public void onEnable() {
+		ajustes = new HashMap<String, Map<String, Double>>();
+		// Registramos comandos:
+		this.getCommand("chetosmobsupdate").setExecutor(new chetosmobsupdate());
+		mobs=new HashSet<String>();
+		//mobs.add("zombie");
 		getLogger().info("Crashyvisioner69 ENABLED");
 	}
 
@@ -13,17 +30,34 @@ public class ChetosMobs extends JavaPlugin {
 		getLogger().info("PUEDES CORRER PERO NO PODRÁS ESCAPAR");
 	}
 
-	public void multiplyUpdater(int k, Monster p, String action) {
-		if (action.toLowerCase().equals("salud")) {
-			p.setHealth(p.getHealth() * k);
+	public void updateHealthMultiplier(double k, Monster p, String action) {
+		p.setHealth(p.getHealth() * k);
+
+	}
+
+	// Vamos por partes, vamos a hacer los comandos, no?
+
+	public class chetosmobsupdate implements CommandExecutor {
+
+		// This method is called, when somebody uses our command
+		// /chetosmobsupdate zombie salud 5
+		public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+			if (true) {//revisar que los argumentos son correctos. de momento no lo hacemos
+				if (ajustes.containsKey(arg3[0])) {
+					ajustes.get(arg3[0]).put(arg3[1], Double.parseDouble(arg3[2]));
+
+				} else {
+					ajustes.put(arg3[0], new HashMap<String, Double>());
+					ajustes.get(arg3[0]).put(arg3[1], Double.parseDouble(arg3[2]));
+				}
+				
+				
+				
+			}else{
+				return false;
+			}
+			return true;
 		}
-		if (action.toLowerCase().equals("daño")) {
-			//MIERDA XD
-		}
-		if( action.toLowerCase().equals("velocidad")){
-			// OH SHIT DOS
-		}
-		
 
 	}
 
